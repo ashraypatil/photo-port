f61987fb60ab733d2dc0b193b943b3bb9b130b16
Ashray <ashraypatil0415@gmail.com>
2023-10-05 02:47:56 -0700

    nav tested

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 import About from '..';
+ afterEach(cleanup);
 
-afterEach(cleanup);
-
-describe('About component', () => {
-  // First Test
+ describe('About component', () => {
+ // baseline test
   it('renders', () => {
     render(<About />);
   });
 
-  // Second Test
-  it('matches snapshot DOM node structure', () => {
-    // You need to render the About component here and use
-    // expect(...) to make assertions about the rendered output.
-    // Example assertion (replace this with your specific assertions):
-    const { container } = render(<About />);
-    expect(container).toMatchSnapshot();
-    
-  });
-});
+ // snapshot test
+ it('matches snapshot', () => {
+ const { container } = render(<About />);
+ expect(container).toMatchSnapshot();
+   });
+ })
